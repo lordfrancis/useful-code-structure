73aae16fd98e7d6246715be69963f0819db2af32
Francis <franznavarswork@gmail.com>
2022-11-30 21:06:03 +0800

    added code structure for jquery

---
+++ useful-code-structure/js/jquery-code-structure.js
@@ -0,0 +1,39 @@
+( function($) {
+	let myJavasript = {
+		init: function() {
+			this.events()
+		},
+
+		events: function() {
+			this.function1();
+			this.function2();
+			this.function3();
+		},
+
+		handleAccordionClick: function() {
+            // If accordion class is clicked, do function.
+			$(document).on('click', '.accordion', function() {
+                // Insert code here.
+			});
+		},
+
+		handleAccordionV2Click: function() {
+            // If accordion-v2 is clicked, do function.
+			$(document).on('click', '.accordion-v2', function() {
+                // Insert code here.
+			});
+		},
+
+		handlePricingTableKeyDown: function() {
+            // IF keydown event happens within toggle-menu class, do function.
+			$( document ).on( 'keydown', '.toggle-menu', function(e) {
+                // Insert code here.
+			} )
+
+		},
+    }
+
+	$(document).ready(function() {
+		myJavasript.init()
+	})
+} )(jQuery)
